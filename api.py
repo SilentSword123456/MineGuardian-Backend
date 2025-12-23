f531b7e3f9c98337284d590469bc93ceb3139562
SilentSword <andrei925.dumitru@gmail.com>
2025-12-23 16:45:47 +0200

    Add CORS support and implement server listing endpoint

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, request
-import psutil
+from flask import Flask, jsonify
 import os
+from flask_cors import CORS
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -13,39 +14,19 @@ def home():
     })
 
 
-@app.route('/resources', methods=['GET'])
-def get_resources():
-    cpuUsage = psutil.cpu_percent(interval=1)
-    memory = psutil.virtual_memory()
-    disk = psutil.disk_usage(DIR)
+@app.route('/servers', methods=['GET'])
+def list_servers():
+    servers = []
+    i = 1
+    for name in os.listdir(os.path.join(DIR, "servers")):
+        servers.append({
+            'name': name,
+            'id': i
+        })
+        i += 1
     return jsonify({
-        'cpu': cpuUsage,
-        'memory': {
-            'total': memory.total,
-            'available': memory.available,
-            'used': memory.used,
-            'percent': memory.percent
-        },
-        'disk': {
-            'total': disk.total,
-            'used': disk.used,
-            'free': disk.free,
-            'percent': disk.percent
-        }
+        'servers': servers
     })
-
-@app.route('/files/<path:location>', methods=['GET'])
-def list_files(location):
-    target_dir = os.path.join(DIR, location)
-    if not os.path.exists(target_dir) or not os.path.isdir(target_dir):
-        return jsonify({'error': 'Directory not found'}), 404
-
-    files = os.listdir(target_dir)
-    return jsonify({
-        'location': location,
-        'files': files
-    })
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
