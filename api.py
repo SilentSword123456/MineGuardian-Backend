01f222df3333da2e853b15e62ba097eac8d3ced9
SilentSword <andrei925.dumitru@gmail.com>
2025-12-23 21:18:27 +0200

    Enhance websocket connection handling with server room support

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 from flask_cors import CORS
 from utils import getConfig
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -37,18 +37,21 @@ def list_servers():
 
 @socketio.on('connect')
 def handle_connect():
-    print('Client connected')
-    emit('message', {'data': 'Connected to server'})
+    serverId = request.args.get('serverId')
+    if(serverId):
+        join_room(serverId)
+        print('Client connected to server room:', serverId)
+        emit('message', {'data': f"Connected to server {serverId}"})
 
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
-@socketio.on('custom_event')
-def handle_custom_event(data):
-    print(f'Received: {data}')
-    # Echo back to client
-    emit('response', {'data': f"Server received: {data['message']}; IT WORKED!!!!!"})
+@socketio.on('message')
+def handleMessage(data):
+    server_id = request.args.get('serverId')
+    print(f'Message from server {server_id}: {data}')
+    emit('message', {'data': f"Server {server_id} received: {data['message']}"})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
