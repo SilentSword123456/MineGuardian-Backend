1db04666e261bd2e7539b98fb8f8920da20180f3
Dumitru Andrei <andrei925.dumitru@gmail.com>
2025-12-24 10:15:14 +0200

    Refactor websocket connection handling to support server name and console commands

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
+import questionary
 from flask import Flask, jsonify, request
 import os
 from flask_cors import CORS
+
+import setup
 from utils import getConfig
 from flask_socketio import SocketIO, emit, join_room
 
@@ -14,6 +17,9 @@ socketio = SocketIO(
     cors_allowed_origins=app.config["SOCKETIO_CORS_ALLOWED_ORIGINS"],
     async_mode="eventlet"
 )
+
+isConsoleActive = False
+serverInstance = None
 
 @app.route('/')
 def home():
@@ -37,22 +43,71 @@ def list_servers():
 
 @socketio.on('connect')
 def handle_connect():
-    serverId = request.args.get('serverId')
-    if(serverId):
-        join_room(serverId)
-        print('Client connected to server room:', serverId)
-        emit('message', {'data': f"Connected to server {serverId}"})
+    serverName = request.args.get('serverName')
+    if(serverName in setup.runningServers):
+        global isConsoleActive
+        global serverInstance
+        isConsoleActive = True
+        serverInstance = setup.runningServers[serverName]
+        join_room(serverName)
+        print('Client connected to server room:', serverName)
+        emit('message', {'data': f"Connected to server {serverName}"})
+
+
 
 @socketio.on('disconnect')
 def handle_disconnect():
+    global serverInstance
+    global isConsoleActive
+    serverInstance = None
+    isConsoleActive = False
     print('Client disconnected')
 
 @socketio.on('message')
 def handleMessage(data):
-    server_id = request.args.get('serverId')
-    print(f'Message from server {server_id}: {data}')
-    emit('message', {'data': f"Server {server_id} received: {data['message']}"})
+    serverName = request.args.get('serverName')
+    print(f'Message from server {serverName}: {data}')
+    emit('message', {'data': f"Server {serverName} received: {data['message']}"})
 
-if __name__ == '__main__':
+@socketio.on('console')
+def handleConsole(data):
+    global serverInstance
+    global isConsoleActive
+    serverName = request.args.get('serverName')
+    if not isConsoleActive:
+        emit('console', {'data': f"Console is not active on server {serverName}."})
+        return
+    if serverInstance is None:
+        serverInstance = setup.getServerInstance(serverName)
+
+    serverInstance.send_command(data['message'])
+    print(f'Message from server {serverName}: {data}')
+
+def startServer(debug=True, port=5000, host="0.0.0.0"):
+    global serverInstance
+    global isConsoleActive
+    socketio.run(app, debug=debug, port=port, host=host)
+
+    while True:
+        if isConsoleActive:
+            if serverInstance is None:
+                serverInstance = setup.getServerInstance(request.args.get('serverName'))
+
+            output = serverInstance._read_output()
+            questionary.print(f"Emitting console data: {output}", style="fg:cyan")
+            emit('console', {'data': serverInstance._read_output()})
+
+def stopServer():
+    socketio.stop()
+"""if __name__ == '__main__':
+    global serverInstance
+    global isConsoleActive
     socketio.run(app, debug=True, port=5000)
+
+    while True:
+        if isConsoleActive:
+            if serverInstance is None:
+                serverInstance = setup.getServerInstance(request.args.get('serverName'))
+
+            emit('console', {'data': serverInstance.output_queue.get()})"""
 
