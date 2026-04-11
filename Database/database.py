8d5ea03d150811817f608e885a844aa8ce1da5d7
SilentSword <andrei925.dumitru@gmail.com>
2026-04-11 22:26:06 +0300

    Add database models for User, FavoriteServers, Player, PlayersPrivileges, and Settings

---
+++ Database/database.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from api import app
+
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class FavoriteServers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    server_id = db.Column(db.Integer)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String)
+    uuid = db.Column(db.String)
+
+class PlayersPrivileges(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+    privilege_id = db.Column(db.Integer)
+
+class Settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    rule = db.Column(db.Integer)
+    approved = db.Column(db.Boolean)
