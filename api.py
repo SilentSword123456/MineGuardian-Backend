dceb3701865ce06daab991b1a3fe95c38ae73257
SilentSword <andrei925.dumitru@gmail.com>
2026-04-15 19:36:16 +0300

    feat: configure JWT token location and cookie settings for authentication

--- api.py
+++ api.py
@@ -20,6 +20,9 @@ app.config.update(getConfig()['flaskConfig'])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mineguardian.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = getConfig()['jwtSecretKey']
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_COOKIE_SECURE"] = False  #TODO True in production
+app.config["JWT_COOKIE_SAMESITE"] = "Lax"
 app.security_schemes = {
     'BearerAuth': {
         'type': 'http',
