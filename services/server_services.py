a8a9365666cb48c960e88bba13174aefbdf3a007
Dumitru Andrei <andrei925.dumitru@gmail.com>
2026-04-16 23:00:17 +0300

    fix: update server functions to use server ID instead of name for consistency

--- services/server_services.py
+++ services/server_services.py
@@ -1,34 +1,27 @@
 import os
 import serverSessionsManager
 import utils
+from Database.perms import ServersPermissions
+from Database.repositories import ServersUsersPermsRepository, ServersRepository
 
 DIR = os.path.dirname(os.path.abspath(__file__ + "/.."))
 
-def get_all_servers():
-    servers = []
-    i = 1
-    for name in os.listdir(os.path.join(DIR, "servers")):
-        servers.append({
-            'server_id': name,
-            'id': i,
-            'isRunning': (
-                serverSessionsManager.serverInstances[name].is_running() if name in serverSessionsManager.serverInstances else False),
-            'max_memory_mb': utils.getMaxMemoryMB(os.path.join(DIR, "servers", name)),
-            'online_players': {'max': utils.getMaxPlayers(os.path.join(DIR, "servers", name))}
-        })
-        i += 1
-    return servers
+def getAllServers(userId: int):
+    serversIds = ServersUsersPermsRepository.getServersWithUserPerm(userId, ServersPermissions.ViewServer.value)
+    return serversIds
 
-def get_server_instance(serverName):
+def get_server_instance(serverId):
+    serverName = ServersRepository.getServerName(serverId)
     if serverName in serverSessionsManager.serverInstances and serverSessionsManager.serverInstances[serverName].is_running():
         raise ValueError(f"Server '{serverName}' is already running")
 
     if serverName not in serverSessionsManager.serverInstances:
-        return utils.setupServerInstance(os.path.join(DIR, "servers", serverName), serverName)
+        return utils.setupServerInstance(os.path.join(DIR, "servers", serverName), serverName, serverId)
 
     return serverSessionsManager.serverInstances[serverName]
 
-def stop_server(serverName):
+def stop_server(serverId):
+    serverName = ServersRepository.getServerName(serverId)
     if serverName not in serverSessionsManager.serverInstances:
         raise ValueError(f"No instance found for Server '{serverName}'")
     serverSessionsManager.serverInstances[serverName].stop()
